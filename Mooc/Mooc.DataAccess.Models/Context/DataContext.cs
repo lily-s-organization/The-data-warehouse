741e1d9ca7e63c8b2d64d6bc465d440b94b2248c
jianglili2015 <jianglili2015@gmail.com>
2019-05-26 19:51:55 +1000

    Mooc Framework

---
+++ Mooc/Mooc.DataAccess.Models/Context/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Mooc.DataAccess.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mooc.DataAccess.Models.Context
+{
+    public class DataContext: DbContext
+    {
+        public DataContext() : base(GetConnectionString())
+        {
+        }
+
+        private static string GetConnectionString()
+        {
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["ConnectionString"];
+            return settings.ConnectionString;
+        }
+
+        public DbSet<User> Users { get; set; }
+
+       
+    }
+}
