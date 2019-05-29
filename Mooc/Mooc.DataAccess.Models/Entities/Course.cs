33679d79d1191f34a4a99f3282304761d170622f
jianglili2015 <jianglili2015@gmail.com>
2019-05-29 19:35:05 +1000

    Update entity classes and note that 1nf-3nf

--- Mooc/Mooc.DataAccess.Models/Entities/Course.cs
+++ Mooc/Mooc.DataAccess.Models/Entities/Course.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +10,16 @@ namespace Mooc.DataAccess.Models.Entities
 {
     public class Course
     {
+        [Key]
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]//添加时自动增长
         public int CourseID { get; set; }
         public string CourseDescription { get; set; }           //课程描述
         public string VideoUrl { get; set; }                    //视频资源路径
-        public int StudyUnit { get; set; }                      //记录学到哪个单元了
-        public long StudyTime { get; set; }                     //记录在某个单元学习了多久 便于下次播放视频定位时间
-        public long TotalLearningHours { get; set; }            //学生在该课程的总学习时长
-        public DateTime? PurchasedTime { get; set; }            //购买时间
-        public bool PurchasedFlag { get; set; }                 //是否购买的标志 如果没购买  则将该课程放入购物车
-
-        public User User { get; set; }
+       // public int StudyUnit { get; set; }                      //记录学到哪个单元了
+       // public long StudyTime { get; set; }                     //记录在某个单元学习了多久 便于下次播放视频定位时间
+       // public long TotalLearningHours { get; set; }            //学生在该课程的总学习时长
+        //public DateTime? PurchasedTime { get; set; }            //购买时间
+       // public bool PurchasedFlag { get; set; }                 //是否购买的标志 如果没购买  则将该课程放入购物车
+      //  public User User { get; set; }
     }
 }
--- Mooc/Mooc.DataAccess.Models/Entities/User.cs
+++ Mooc/Mooc.DataAccess.Models/Entities/User.cs
@@ -11,7 +11,6 @@ namespace Mooc.DataAccess.Models.Entities
     {
         [Key]
         [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]//添加时自动增长
-                                                                                                          // public long Id { get; set; }
         public long Id { get; set; }
 
         [Required]
@@ -25,41 +24,37 @@ namespace Mooc.DataAccess.Models.Entities
         [DataType(DataType.Password)]
         public string PassWord { get; set; }
 
-        [Required(ErrorMessage = "Please re-type your password")]
-        [Compare("PassWord", ErrorMessage = "password and re-type password must match")]
-        [Display(Name = "确认密码")]
-        [DataType(DataType.Password)]
-        public string Confirm { get; set; }
+        //[Required(ErrorMessage = "Please re-type your password")]
+        //[Compare("PassWord", ErrorMessage = "password and re-type password must match")]
+        //[Display(Name = "确认密码")]
+        //[DataType(DataType.Password)]
+        //public string Confirm { get; set; }
 
         [Required]
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9]+\.[A-Za-z]{2,4}", ErrorMessage = "{0}的格式不正确")]
+       // [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9]+\.[A-Za-z]{2,4}", ErrorMessage = "{0}的格式不正确")]
         [Display(Name = "邮箱")]
-        [DataType(DataType.EmailAddress)]
+        [DataType(DataType.EmailAddress)]//已经存在邮件的校验了
         public string Email { get; set; }
 
-        [Display(Name = "昵称")]
-        public string NickName { get; set; }
-
-
-
-
+        [Display(Name = "头像")]
+        public string PhotoUrl { get; set; }
 
         public int UserState { get; set; }
         [Required]
-        [Range(0, 1, ErrorMessage = " 字段 RoleType 必须在 0 和 1 之间,学生选0，老师选1")]
+        [Range(1, 3)]//1=管理员 2=老师 3=学生
         public int RoleType { get; set; }       //通过枚举来区别是学生还是老师
         public DateTime? AddTime { get; set; }       //用户注册时间
-        public int Credit { get; set; }             //用户积分
-        public int LearningHours { get; set; }      //用户总学习时长
-        public string PhotoUrl { get; set; }        //用户照片
-        public int Phone { get; set; }
+       // public int Credit { get; set; }             //用户积分
+       // public int LearningHours { get; set; }      //用户总学习时长
+       // public string PhotoUrl { get; set; }        //用户照片
+       // public int Phone { get; set; }
 
-        public List<Follower> FollowingsList { get; set; }          //关注列表
-        public List<Follower> FollowedList { get; set; }            //粉丝列表
+      //  public List<Follower> FollowingsList { get; set; }          //关注列表
+      //  public List<Follower> FollowedList { get; set; }            //粉丝列表
 
 
-        public List<Course> CourseList { get; set; }
-        public List<Comment> CommentList { get; set; }         //在一门课程中，学生可以提问很多次
+      //  public List<Course> CourseList { get; set; }
+      //  public List<Comment> CommentList { get; set; }         //在一门课程中，学生可以提问很多次
 
     }
 }
