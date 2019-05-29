a67da85abcb6bf2aeeb54f44292d409ce7655ca0
jianglili2015 <jianglili2015@gmail.com>
2019-05-29 19:32:31 +1000

    Play MVC using these two demo controllers

---
+++ Mooc/Mooc.Web.UI/Controllers/Demo1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mooc.Web.UI.Controllers
+{
+    public class Demo1Controller : Controller
+    {
+        // 使用 razor 绑定数据实现 User 的增删改查  EF + Automap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mooc/Mooc.Web.UI/Controllers/Demo2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mooc.Web.UI.Controllers
+{
+    public class Demo2Controller : Controller
+    {
+        // 使用 jquey / vue.js /angular.js 绑定数据 EF+automap+autofac (不要急)
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
