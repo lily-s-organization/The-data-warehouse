5a182ffe91449eb1ac5a0d03302642303bcf32c4
jianglili2015 <jianglili2015@gmail.com>
2019-05-29 19:37:00 +1000

    Add viewmodels

--- Mooc/Mooc.DataAccess.Models/ViewModels/UserViewModel.cs
+++ Mooc/Mooc.DataAccess.Models/ViewModels/UserViewModel.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using Mooc.DataAccess.Models.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mooc.DataAccess.Models.ViewModels
 {
-    public class UserViewModel
+    public class UserViewModel:User
     {
-        public long Id { get; set; }
-        public string UserName { get; set; }
-        public string NickName { get; set; }
-        public int UserState { get; set; }
-        public int RoleType { get; set; }
+
+        [Required(ErrorMessage = "Please re-type your password")]
+        [Compare("PassWord", ErrorMessage = "password and re-type password must match")]
+        [Display(Name = "确认密码")]
+        [DataType(DataType.Password)]
+        public string Confirm { get; set; }
     }
 }
