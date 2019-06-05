712a4927d34bafe7138bc556ff55b96048732834
Eric <zhoucapacity@gmail.com>
2019-06-05 10:48:10 +0800

    添加 Login Page

--- Mooc/Mooc.Web.UI/App_Start/RouteConfig.cs
+++ Mooc/Mooc.Web.UI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Mooc.Web.UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Users", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
--- Mooc/Mooc.Web.UI/Controllers/UsersController.cs
+++ Mooc/Mooc.Web.UI/Controllers/UsersController.cs
@@ -24,6 +24,10 @@ namespace Mooc.Web.UI.Controllers
             return View();
         }
 
+        public ActionResult Login()
+        {
+            return View();
+        }
 
         public ActionResult Add()
         {
