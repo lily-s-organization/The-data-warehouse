ab2ff5e8a67e42d076c3b87b3bf2a0dc8d28e67c
Eric <zhoucapacity@gmail.com>
2019-06-07 11:40:35 +0800

    调整在过滤器中验证用户权限的逻辑

--- Mooc/Mooc.Web.UI/Filter/LoginAuthorizeAttribute.cs
+++ Mooc/Mooc.Web.UI/Filter/LoginAuthorizeAttribute.cs
@@ -11,20 +11,27 @@ namespace Mooc.Web.UI.Filter
     {
         public override void OnAuthorization(AuthorizationContext filterContext)
         {
+            //跳过免验证的controller
             if (filterContext.ActionDescriptor.IsDefined(typeof(AllowAnonymousAttribute), true)
                || filterContext.ActionDescriptor.ControllerDescriptor.IsDefined(typeof(AllowAnonymousAttribute), true))
             {
                 return;
             }
 
-          
+            //得到前端发来的cookie值
+            HttpCookie cookie = filterContext.HttpContext.Request.Cookies.Get(CommonVariables.LoginCookieName);
+            var cookieUserName = cookie.Value;
 
             //判断登录情况
-            if (CookieHelper.GetCookie(CommonVariables.LoginCookieName) == "")
+            if ((cookieUserName == null) ||(CookieHelper.GetCookie(CommonVariables.LoginCookieName) != cookieUserName))
             {
-                //cookie里没有值 用户没有登录  没有权限访问
+                //cookie里没有值 或者值不对  没有权限访问
 
                 filterContext.Result = new RedirectResult("/Users/Login");
+            }
+            else
+            {
+                return;
             }
         }
 
