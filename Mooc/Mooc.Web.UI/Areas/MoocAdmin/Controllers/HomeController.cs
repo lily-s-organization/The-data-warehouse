6cf98b1ea16a1cd5c5c300b2e424252bd5d5b5a1
jianglili2015 <jianglili2015@gmail.com>
2019-06-12 17:54:38 +1000

    Add area for mooc admin

---
+++ Mooc/Mooc.Web.UI/Areas/MoocAdmin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mooc.Web.UI.Areas.MoocAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: MoocAdmin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mooc/Mooc.Web.UI/Areas/MoocAdmin/MoocAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Mooc.Web.UI.Areas.MoocAdmin
+{
+    public class MoocAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "MoocAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "MoocAdmin_default",
+                "MoocAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
