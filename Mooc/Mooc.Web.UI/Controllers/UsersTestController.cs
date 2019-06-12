ef743548b7d1e9bd26ecd46dd561fff9028386d5
jianglili2015 <jianglili2015@gmail.com>
2019-06-12 18:37:07 +1000

    Add exmple about how to use model validation

---
+++ Mooc/Mooc.Web.UI/Controllers/UsersTestController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mooc.DataAccess.Models.Context;
+using Mooc.DataAccess.Models.Entities;
+
+namespace Mooc.Web.UI.Controllers
+{
+    public class UsersTestController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: UsersTest
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        // GET: UsersTest/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: UsersTest/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsersTest/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserName,PassWord,Email,PhotoUrl,UserState,RoleType,AddTime")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var u = db.Users.FirstOrDefault(x=>x.UserName==user.UserName);
+                if (u != null && u.Id > 0)
+                {
+                    ModelState.AddModelError("", "用户已经存在");
+                    return View(user);
+                }
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: UsersTest/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: UsersTest/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserName,PassWord,Email,PhotoUrl,UserState,RoleType,AddTime")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: UsersTest/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: UsersTest/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
