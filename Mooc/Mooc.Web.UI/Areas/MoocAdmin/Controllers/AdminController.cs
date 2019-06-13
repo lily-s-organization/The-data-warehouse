8d055fc8418eef2175fd6208a71762a49fea94f5
Eric <zhoucapacity@gmail.com>
2019-06-13 20:18:49 +0800

    为area后台添加用户信息展示页 使用bootstrap样式

---
+++ Mooc/Mooc.Web.UI/Areas/MoocAdmin/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mooc.Web.UI.Areas.MoocAdmin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: MoocAdmin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
