263fe1ee2bbaf0b91a58b0dbb390592870e95382
Eric <zhoucapacity@gmail.com>
2019-06-15 14:19:22 +0800

    添加 展示用户页

---
+++ Mooc/Mooc.Web.UI/Areas/MoocAdmin/Controllers/UsersController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net;
+using Mooc.DataAccess.Models.Context;
+using Mooc.DataAccess.Models.Entities;
+using Mooc.Web.UI.Filter;
+
+namespace Mooc.Web.UI.Areas.MoocAdmin.Controllers
+{
+    //[LoginAuthorize]
+    public class UsersController : Controller
+    {
+        private DataContext db = new DataContext();
+        private ILog logger = LogManager.GetLogger(typeof(UsersController));
+        // GET: MoocAdmin/Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AddUserList(User user)
+        {
+            try
+            {
+                if (user == null)
+                    return Json(300);
+
+                if (user.Id == 0)
+                {
+                    user.UserState = 0;
+                    user.AddTime = DateTime.Now;
+                    db.Users.Add(user);
+                }
+                else
+                {
+                    db.Entry(user).State = EntityState.Modified;
+                }
+
+                db.SaveChanges();
+                return Json(0);           
+            }
+            catch (Exception ex)
+            {
+
+                logger.Error(ex.Message);
+                return Json(400);
+            }
+
+
+
+        }
+    }
+}
