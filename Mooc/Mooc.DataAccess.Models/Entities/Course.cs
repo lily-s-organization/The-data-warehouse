dcdaef391f9f3967bbd0a7b1c91694762318d393
Eric <zhoucapacity@gmail.com>
2019-06-19 13:03:41 +0800

    数据库更新

--- Mooc/Mooc.DataAccess.Models/Entities/Course.cs
+++ Mooc/Mooc.DataAccess.Models/Entities/Course.cs
@@ -15,11 +15,11 @@ namespace Mooc.DataAccess.Models.Entities
         public int CourseID { get; set; }
         public string CourseDescription { get; set; }           //课程描述
         public string VideoUrl { get; set; }                    //视频资源路径
-       // public int StudyUnit { get; set; }                      //记录学到哪个单元了
-       // public long StudyTime { get; set; }                     //记录在某个单元学习了多久 便于下次播放视频定位时间
-       // public long TotalLearningHours { get; set; }            //学生在该课程的总学习时长
+        // public int StudyUnit { get; set; }                      //记录学到哪个单元了
+        // public long StudyTime { get; set; }                     //记录在某个单元学习了多久 便于下次播放视频定位时间
+        // public long TotalLearningHours { get; set; }            //学生在该课程的总学习时长
         //public DateTime? PurchasedTime { get; set; }            //购买时间
-       // public bool PurchasedFlag { get; set; }                 //是否购买的标志 如果没购买  则将该课程放入购物车
-      //  public User User { get; set; }
+        // public bool PurchasedFlag { get; set; }                 //是否购买的标志 如果没购买  则将该课程放入购物车
+        //  public User User { get; set; }
     }
 }
