7654d6d4c42a2eeceae57f500635e5f560c6eb6c
Eric <zhoucapacity@gmail.com>
2019-06-26 15:02:15 +0800

    章节添加页面(只有css样式，上传视频功能未完成)

---
+++ Mooc/Mooc.Web.UI/Areas/MoocAdmin/Controllers/SectionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mooc.Web.UI.Areas.MoocAdmin.Controllers
+{
+    public class SectionController : Controller
+    {
+        // GET: MoocAdmin/Section
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
